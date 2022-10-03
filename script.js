3f00d4211da86ff531b9070610969d1c213311ec
Loooai10 <honey_moonn@yahoo.com>
2022-10-03 13:52:32 +0300

    updated

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const boardCells = document.querySelectorAll('.board-cell');
 const turn = document.querySelector('.turn');
 const result = document.querySelector('.result');
+//document.body.style.backgroundImage = 'none';
